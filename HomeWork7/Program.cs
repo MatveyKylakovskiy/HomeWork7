56f6cf2186937f1efd5348adcf1bbd8d3134a9f7
Matveykylak <matveykylak@gmail.com>
2024-04-06 16:29:39 +0300

    First task

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using HomeWork7;
+
+var green = new Square(5, "green");
+var blue = new Square(2, "blue");
+var lightBlue = new Square(15, "light blue");
+
+
+green.GetInfo();
+blue.GetInfo();
+lightBlue.GetInfo();
+
+green.Rewriter(green, blue);
+green.GetInfo();
+
