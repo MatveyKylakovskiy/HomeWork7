1f7ed60bf904817b8d09951d95b2e7f185fd7810
Matveykylak <matveykylak@gmail.com>
2024-04-07 15:27:27 +0300

    Second Task

---
+++ HomeWork7/TypeOfTreatment.cs
@@ -0,0 +1,27 @@
+﻿
+namespace HomeWork7
+{
+    public class TypeOfTreatment
+    {
+        public int NumberOfTreatment;
+
+        public Doctor ChoiceOfTreatment(int NumberOfTreatment)
+        {
+          switch(NumberOfTreatment)
+            {
+                case 1:
+                    return new Surgeon();
+                case 2:
+                    return new Dentist();
+                default:
+                    return new Therapist();
+            }
+
+        }
+
+        public TypeOfTreatment(int NumberOfTreatment)
+        {
+            this.NumberOfTreatment = NumberOfTreatment;
+        }
+    }
+}
